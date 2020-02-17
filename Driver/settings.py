4d1599dac772bb35fb3752a05020279b8c3ede53
Gribek <piotrekmaslewski@gmail.com>
2020-02-17 13:37:17 +0100

    db update.

--- Driver/settings.py
+++ Driver/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'drive_safe',
         'USER': 'postgres',
-        'PASSWORD': 'coderslab',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
     }
 }
